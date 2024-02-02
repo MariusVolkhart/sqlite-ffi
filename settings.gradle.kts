05abbf2ef7d3fa186df236b59e7e08eb925fd9d9
Marius Volkhart <marius@volkhart.com>
2024-02-02 11:16:14 -0500

    Upgrade Sqlite to 3.45.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ dependencyResolutionManagement {
 	repositories {
 		mavenCentral()
 		ivy {
-			url = uri("https://sqlite.org/2022/")
+			url = uri("https://sqlite.org/2024/")
 			patternLayout {
 				artifact("[organisation]-[module]-[revision].zip")
 			}
